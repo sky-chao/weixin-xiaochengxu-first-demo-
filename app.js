91607ee6325963b7f77f9d685908ce1c761a4298
tc <121261274@qq.com>
2018-04-28 10:59:45 +0800

    音乐播放完成，但是有一个小bug就是当点击编辑器的模拟网页按钮，不继续播放，而是从头开始，全局的按钮就是正常显示

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 App({
   globalData:{
-    g_isplaymusic:false //全局控制音乐播放的状态
+    g_isplaymusic:false ,//全局控制音乐播放的状态
+    g_isplaymusicId:null //正在播放的那首歌的Id
   }
 })
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,147 +1,148 @@
 import lists from "../../../data/posts-data.js";
-var app=getApp();
+var app = getApp();
 var g_isplay = app.globalData.g_isplaymusic;//全局播放状态
 Page({
-  data: {
-    isplaymusic: false
-  },
-  //初始化
-  onLoad(option) {
-    //option参数中有点击传入的get参数id
-    var postId = option.id;
-    this.setData({
-      lists: lists.postList[postId],//获取通过id及上边import引进对象中数组的嵌套数据
-      postId
-    })
-    // var post_Collections={
-    //     0:false,
-    //     3:true
-    // }
-    //获取存储在本地的收藏状态
-    var postCollections = wx.getStorageSync("post_Collections");
-    if (postCollections) {
-      var CollectionId = postCollections[postId];//得到的值 true/false
-      this.setData({
-        Collection: CollectionId//方便显示wxml显示那张图片
-      })
-    } else {
-      var postCollections = {};
-      postCollections[postId] = false;
-      wx.setStorageSync("post_Collections", postCollections);
-    }
-
-
-    //解决手机全局播放与小程序播放器图片按钮显示一致 （bug 已解决）
-    var that=this;
-    wx.onBackgroundAudioPlay(function () {
-      that.setData({
-        isplaymusic: true
-      })
-    });//监听音乐播放
-    wx.onBackgroundAudioPause(function () {
-      that.setData({
-        isplaymusic:false
-      })
-    })//监听音乐暂停。
-
-
-    //如果初始全局音乐播放状态为true，就改变此页面的音乐初始状态,在局部改变状态时，通知全局（播放与暂停）
-    if (app.globalData.g_isplaymusic){
-      this.setData({
-        isplaymusic:true
-      })
-    }
-  },
-  //点击收藏与取消收藏
-  onCollection() {
-    var postCollections = wx.getStorageSync("post_Collections");
-    this.showToast(postCollections);
-  },
-  //用户有提示没有选择权的提示框
-  showToast(postCollections) {
-    //当点击收藏，就是给收藏按钮状态取反（缓存）
-    postCollections[this.data.postId] = !postCollections[this.data.postId]
-    wx.setStorageSync("post_Collections", postCollections);
-    //同时也要初始化收藏按钮图片显示状态
-    this.setData({
-      Collection: postCollections[this.data.postId]
-    })
-    //提醒用户是否收藏/取消成功 ，用到的API:wx.showToast(OBJECT)
-    wx.showToast({
-      title: postCollections[this.data.postId] ? "收藏成功" : "取消成功",
-      icon: "success",
-      duration: 1000
-    });
-  },
-  //魔太框,再次提示用户收藏与否
-  showModal(postCollections) {
-    var that = this;
-    wx.showModal({
-      title: "提示",
-      cancelText: "取消",
-      confirmText: "确定",
-      content: postCollections[that.data.postId] ? "是否取消收藏？" : "是否收藏？",
-      success(res) {
-        if (res.confirm) {
-          //当点击收藏，就是给收藏按钮状态取反（缓存）
-          postCollections[that.data.postId] = !postCollections[that.data.postId]
-          wx.setStorageSync("post_Collections", postCollections);
-          //同时也要初始化收藏按钮图片显示状态
-          that.setData({
-            Collection: postCollections[that.data.postId]
-          })
+    data: {
+        isplaymusic: false
+    },
+    //初始化
+    onLoad(option) {
+        //option参数中有点击传入的get参数id
+        var postId = option.id;
+        this.setData({
+            lists: lists.postList[postId],//获取通过id及上边import引进对象中数组的嵌套数据
+            postId
+        })
+        // var post_Collections={
+        //     0:false,
+        //     3:true
+        // }
+        //获取存储在本地的收藏状态
+        var postCollections = wx.getStorageSync("post_Collections");
+        if (postCollections) {
+            var CollectionId = postCollections[postId];//得到的值 true/false
+            this.setData({
+                Collection: CollectionId//方便显示wxml显示那张图片
+            })
+        } else {
+            var postCollections = {};
+            postCollections[postId] = false;
+            wx.setStorageSync("post_Collections", postCollections);
         }
 
-      }
-    })
 
-  },
-  //用户点击分享时（​显示操作菜单）
-  onsharetap(e) {
-    //显示操作菜单
-    wx.showActionSheet({
-      itemList: ['发QQ朋友圈', '发QQ好友', '发微信朋友圈', '发微信好友'],
-      itemColor: "#b3d4db",
-      success: function (res) {
-        //res.tapIndex数组元素的序号从0开始（itemList）
-        wx.showModal({
-          title: '分享至',
-          content: '真的要分享到',
-          fail(res) {
-            console.log(res)
-          }
+        //解决手机全局播放与小程序播放器图片按钮显示一致 （bug 已解决）
+        var that = this;
+        wx.onBackgroundAudioPlay(function () {
+            that.setData({
+                isplaymusic: true
+            });
+            app.globalData.g_isplaymusic = true;//改变全局状态
+            app.globalData.g_isplaymusicId = that.data.postId;//监听正在播放歌曲的Id
+        });//监听音乐播放
+        wx.onBackgroundAudioPause(function () {
+            that.setData({
+                isplaymusic: false
+            })
+            app.globalData.g_isplaymusic= false;
+            app.globalData.g_isplaymusicId = null;//监听暂停时：没有播放的歌曲
+        })//监听音乐暂停。
+
+
+        //如果初始全局音乐播放状态为true 且是监听到的那个Id，就改变此页面的音乐初始状态,在局部改变状态时，通知全局（监听有改变就改动）
+        if (app.globalData.g_isplaymusic && app.globalData.g_isplaymusicId === this.data.g_isplaymusicId) {
+            this.setData({
+                isplaymusic: true
+            })
+        }
+    },
+    //点击收藏与取消收藏
+    onCollection() {
+        var postCollections = wx.getStorageSync("post_Collections");
+        this.showToast(postCollections);
+    },
+    //用户有提示没有选择权的提示框
+    showToast(postCollections) {
+        //当点击收藏，就是给收藏按钮状态取反（缓存）
+        postCollections[this.data.postId] = !postCollections[this.data.postId]
+        wx.setStorageSync("post_Collections", postCollections);
+        //同时也要初始化收藏按钮图片显示状态
+        this.setData({
+            Collection: postCollections[this.data.postId]
         })
-      }
-    })
-  },
-  //背景音乐的播放
-  onmiusictap() {
-    var a = this.data.isplaymusic
-    var that = this;
-    var postItemData = lists.postList[that.data.postId];
-    // var g_isplay = app.globalData.g_isplaymusic;
-    if (a) {
-      wx.pauseBackgroundAudio();
-      this.setData({
-        isplaymusic: false
-      })
-      g_isplay=false;
+        //提醒用户是否收藏/取消成功 ，用到的API:wx.showToast(OBJECT)
+        wx.showToast({
+            title: postCollections[this.data.postId] ? "收藏成功" : "取消成功",
+            icon: "success",
+            duration: 1000
+        });
+    },
+    //魔太框,再次提示用户收藏与否
+    showModal(postCollections) {
+        var that = this;
+        wx.showModal({
+            title: "提示",
+            cancelText: "取消",
+            confirmText: "确定",
+            content: postCollections[that.data.postId] ? "是否取消收藏？" : "是否收藏？",
+            success(res) {
+                if (res.confirm) {
+                    //当点击收藏，就是给收藏按钮状态取反（缓存）
+                    postCollections[that.data.postId] = !postCollections[that.data.postId]
+                    wx.setStorageSync("post_Collections", postCollections);
+                    //同时也要初始化收藏按钮图片显示状态
+                    that.setData({
+                        Collection: postCollections[that.data.postId]
+                    })
+                }
 
-    } else {
-      wx.playBackgroundAudio({
-        dataUrl: postItemData.music.url,
-        title: postItemData.music.title,
-        coverImgUrl: postItemData.music.coverImg
-      })
-      this.setData({
-        isplaymusic: true
-      })
-      g_isplay = true;
+            }
+        })
+
+    },
+    //用户点击分享时（​显示操作菜单）
+    onsharetap(e) {
+        //显示操作菜单
+        wx.showActionSheet({
+            itemList: ['发QQ朋友圈', '发QQ好友', '发微信朋友圈', '发微信好友'],
+            itemColor: "#b3d4db",
+            success: function (res) {
+                //res.tapIndex数组元素的序号从0开始（itemList）
+                wx.showModal({
+                    title: '分享至',
+                    content: '真的要分享到',
+                    fail(res) {
+                        console.log(res)
+                    }
+                })
+            }
+        })
+    },
+    //背景音乐的播放
+    onmiusictap() {
+        var a = this.data.isplaymusic
+        var that = this;
+        var postItemData = lists.postList[that.data.postId];
+
+        if (a) {
+            wx.pauseBackgroundAudio();
+            this.setData({
+                isplaymusic: false
+            })
+        } else {
+            wx.playBackgroundAudio({
+                dataUrl: postItemData.music.url,
+                title: postItemData.music.title,
+                coverImgUrl: postItemData.music.coverImg
+            })
+            this.setData({
+                isplaymusic: true
+            })
+        }
+        this.setData({
+            isplaymusic: !a
+        })
     }
-    this.setData({
-      isplaymusic: !a
-    })
-  }
 
 
 })
